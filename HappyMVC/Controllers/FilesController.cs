3ffd6f6fda2c9de1dbad27e9a3e22d8f25966624
Stephen Cheng <aw3someone@gmail.com>
2017-09-13 14:18:51 -0700

    moved TextFiles to App_Data

--- HappyMVC/Controllers/FilesController.cs
+++ HappyMVC/Controllers/FilesController.cs
@@ -13,14 +13,14 @@ namespace HappyMVC.Controllers
         public ActionResult Index()
         {
             var names = Directory
-                .EnumerateFiles(Server.MapPath("~/TextFiles"), "*", SearchOption.AllDirectories)
+                .EnumerateFiles(Server.MapPath("~/App_Data/TextFiles"), "*", SearchOption.AllDirectories)
                 .Select(Path.GetFileNameWithoutExtension);
             return View(names);
         }
 
         public ActionResult Contents(String id)
         {
-            String txt = System.IO.File.ReadAllText(Server.MapPath($"~/TextFiles/{id}.txt"));
+            String txt = System.IO.File.ReadAllText(Server.MapPath($"~/App_Data/TextFiles/{id}.txt"));
             ViewBag.Contents = txt;
             return View();
         }
