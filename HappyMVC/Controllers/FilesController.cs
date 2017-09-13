24f24e94211f8e52ec6e31b6399c38e5a58b42f7
Stephen Cheng <aw3someone@gmail.com>
2017-09-13 13:51:24 -0700

    files controller

---
+++ HappyMVC/Controllers/FilesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HappyMVC.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            var names = Directory
+                .EnumerateFiles(Server.MapPath("~/TextFiles"), "*", SearchOption.AllDirectories)
+                .Select(Path.GetFileNameWithoutExtension);
+            return View(names);
+        }
+
+        public ActionResult Contents(String id)
+        {
+            String txt = System.IO.File.ReadAllText(Server.MapPath($"~/TextFiles/{id}.txt"));
+            ViewBag.Contents = txt;
+            return View();
+        }
+    }
+}
